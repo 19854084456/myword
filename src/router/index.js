2681c58ab3ecf32ca0f5938f4bba5207c773b094
19854084456 <19854084456@163.com>
2025-08-04 18:07:04 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,40 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeLayout from './../page/homeLayout.vue'
 import ProjectsPage from './../page/onlineResume/personalAdvantage.vue'
+import SmartNotes from './../page/smartNotes/index.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    redirect: '/homeLayout'
+    redirect: '/homeLayout',
+    meta: {
+      title: '陈南方'
+    }
   },
   {
     path: '/homeLayout',
     name: 'HomeLayout',
-    component: HomeLayout
+    component: HomeLayout,
+    meta: {
+      title: '陈南方'
+    }
   },
   {
     path: '/projectsPage',
     name: 'projectsPage',
-    component: ProjectsPage
+    component: ProjectsPage,
+    meta: {
+      title: '个人简历'
+    }
+  },
+  {
+    path: '/smartNotes',
+    name: 'smartNotes',
+    component: SmartNotes,
+    meta: {
+      title: '智慧笔记'
+    }
   }
 ]
 
@@ -25,6 +43,7 @@ const router = createRouter({
   routes
 })
 router.afterEach((to) => {
+  document.title = to.meta.title
   if (typeof _hmt !== "undefined") {
     _hmt.push(["_trackPageview", to.fullPath]); // 发送新页面路径
   }
